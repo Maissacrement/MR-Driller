bd780708ea56627b2e37f7c13ac0bed978616f2f
anatole <anatole.janvion@gmail.com>
2019-04-17 07:30:01 +0200

    update

--- Block.py
+++ Block.py
@@ -1,9 +1,40 @@
-# Creation d'un Block
 class Block():
+
+    def __init__(self):
+        self.title = "Title"
+
+    def getTitle(self):
+        return self.title
+
+
+print(Block().getTitle())
+
+class Block():
+
     """Propriete generale d'un Block."""
-    def __init__(self, nom):
+
+    def _init_(self,couleur,positionx,positiony):
         """Constante"""
-        self.DIMENSION = 5,5
-        self.FORCE = 5
-        """Variable"""
-        self.nom = nom
+        self.couleur = couleur
+        if type(couleur) is not str:
+            raise ValueError
+        self.positionx = positionx
+        if type(positionx) is not int:
+            raise ValueError
+        self.positiony positiony
+        if type(positiony) is not int:
+            raise ValueError
+        vie = int()
+        if type(vie) is not int:
+            raise ValueError
+
+    """fonction verification si block est detruit ou pas """
+    def isDead():
+        #bool qui
+        estvivant = True
+        if vie <= 0:
+            estvivant = False
+        return boolvivant
+
+    """fonction qui tcheck si il y a un block autour du block"""
+    def nearOf(positionx,positiony):
