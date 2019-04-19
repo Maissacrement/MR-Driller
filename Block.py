0b2dbc454544514cb5a56fadac2f1a510251a03a
mario <emmariodelar@gmail.com>
2019-04-19 20:04:35 +0000

    Objects

--- Block.py
+++ Block.py
@@ -1,9 +1,37 @@
-# Creation d'un Block
 class Block():
+
     """Propriete generale d'un Block."""
-    def __init__(self, nom):
+
+    def _init_(self,couleur,positionx,positiony):
         """Constante"""
-        self.DIMENSION = 5,5
-        self.FORCE = 5
-        """Variable"""
-        self.nom = nom
+        self.couleur = couleur
+        if type(couleur) is not str:
+            raise ValueError
+        self.positionx = positionx
+        if type(positionx) is not int:
+            raise ValueError
+        self.positiony = positiony
+        if type(positiony) is not int:
+            raise ValueError
+        vie = int()
+        if type(vie) is not int:
+            raise ValueError
+
+    """fonction qui detruit le l'Object block """
+    def __del__():
+        print("le block a ete detruit")
+
+
+    """fonction verification si block est detruit ou pas """
+    def isDead():
+        # vartiable bool qui renvoie si le block est vivaant ou mort
+        estvivant = True
+
+        #vérification de la condition de mort
+        if vie <= 0:
+            estvivant = False
+        return boolvivant
+
+    """fonction qui tcheck si il y a un block autour du block"""
+    def nearOf(positionx,positiony):
+        print("positionx")
---
+++ Functions/createGoodBlock.py
@@ -0,0 +1,34 @@
+from Objects.Block import * # Recuperer l'object Block
+
+"""
+    Renvoie un Block SPECIAL si isSpecial est vrai
+    ou un block normal sinon
+    --------------------------------
+    @params:
+        Dict : getRandomColor() {
+            Couleur : String
+            isSpecial : Bool
+        }
+        Position : Array<Int,Int>
+            coordonne [x,y]
+        Vie : Int
+        merge : Bool
+    --------------------------------
+    @return: Object {
+        Block
+    }
+"""
+def createGoodBlock(Dict, position, vie=1, merge=True):
+    # Si il s'agit d'un block special nous devrons gerer les different cas
+    # Avec des conditions
+    if Dict['isSpecial']:
+        if Dict['color'] == "brown":
+            return Block(Dict['color'], position, vie=5) # Si le Block est marrons il a 5 vie
+        if Dict['color'] == "white":
+            return Block(Dict['color'], position, vie, False) # Les blocks blancs ne peuvent pas fusionner entre eux
+        if Dict['color'] == "crystal":
+            return Block(Dict['color'], position, vie) # Pas finie
+
+    #Sinon on garde les propriete general d'un block
+    else:
+        return Block(Dict['color'], position, vie)
